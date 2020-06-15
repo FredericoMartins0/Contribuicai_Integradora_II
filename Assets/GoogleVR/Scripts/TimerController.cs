f85ac7e9ede46c9767f583e760e1b3afd0015359
FredericoMartins0 <frederico.martins@edu.pucrs.br>
2020-06-15 13:29:59 -0300

    Menu 180 graus adicionado com Timer

--- Assets/GoogleVR/Scripts/TimerController.cs
+++ Assets/GoogleVR/Scripts/TimerController.cs
@@ -6,21 +6,45 @@ using UnityEngine.UI;
 public class TimerController : MonoBehaviour
 {
     public bool press = false;
+    public bool timing = true;
 
     static float timer = 0.0f;
     public Text text_box;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (timing)
+        {
+            UpdateTimer();
+        }
+    }
+
+    void UpdateTimer()
+    {
         timer += Time.deltaTime;
         var min = Mathf.Floor(timer / 60);
         var sec = timer % 60;
         text_box.text = string.Format("{0:00} : {1:00}", min, sec);
+    }
+
+    public void StartTimer()
+    {
+        timing = true;
+    }
+
+    public void StopTimer()
+    {
+        timing = false;
+    }
+
+    public void ResetTime()
+    {
+        timer = 0.0f;
+        
     }
 }
--- README.md
+++
@@ -1,2 +0,0 @@
-# Contribuicao_Integradora_II
-Implementação do aplicativo em Unity para o segundo trabalho da Disciplina Integradora II
