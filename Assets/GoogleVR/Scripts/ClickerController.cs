fbed8c9ed6fd9ef4571809070e535597e626f414
FredericoMartins0 <frederico.martins@edu.pucrs.br>
2020-06-18 08:48:05 -0300

    Timer e contador de cliques ajustados

---
+++ Assets/GoogleVR/Scripts/ClickerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickerController : MonoBehaviour
+{
+    public static int click = 0;
+    public Text text_box;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text_box.text = string.Format("{0}", click);
+    }
+
+    public void Clique()
+    {
+            click += 1;
+    }
+
+    public void ResetClique()
+    {
+        click = 0;
+        text_box.text = string.Format("{0}", click);
+    }
+}
--- Assets/GoogleVR/Scripts/TimerController.cs
+++ Assets/GoogleVR/Scripts/TimerController.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class TimerController : MonoBehaviour
 {
     public bool press = false;
-    public bool timing = true;
+    public bool timing = false;
 
     static float timer = 0.0f;
     public Text text_box;
@@ -45,6 +45,8 @@ public class TimerController : MonoBehaviour
     public void ResetTime()
     {
         timer = 0.0f;
-        
+        var min = Mathf.Floor(timer / 60);
+        var sec = timer % 60;
+        text_box.text = string.Format("{0:00} : {1:00}", min, sec);
     }
 }
